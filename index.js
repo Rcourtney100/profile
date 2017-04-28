fc7db576c62b989e44af0ba3b1c31848b18d07eb
Ryan Courtney <ryan.courtney1@gmail.com>
2017-04-28 11:26:44 -0400

    added more animations

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ $( document ).ready(function() {
 	$(window).load(function() {
 $('#banner').addClass('animated fadeInLeft');
 $('#jumbotron').addClass('animated fadeInLeft')
+$('#social-icons').addclass('animated fadeInLeft')
 
-})});
+})});s
